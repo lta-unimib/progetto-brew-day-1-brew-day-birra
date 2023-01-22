c36cf47bb1f69c37a1297b17f309ee41f541c8bc
qazimtoska <q.toska@campus.unimib.it>
2023-01-22 11:35:45 +0100

    React Navigation Component added to layout

--- frontend/brew-day/src/App.js
+++ frontend/brew-day/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <React.Fragment>
+      <NavBar />
     </React.Fragment>
   );
 }
---
+++ frontend/brew-day/src/NavBar.jsx
@@ -0,0 +1,13 @@
+function NavBar() {
+    return(
+        <ul>
+            <li><a href="#"><img src="../icons/logo1.png" height="50px"></img></a></li>
+            <li><a href="#">Ricette</a></li>
+            <li><a href="#">Birre</a></li>
+            <li><a href="#">Inventario</a></li>
+            <li><a href="#">Spesa</a></li>
+        </ul>
+    );
+}
+
+export default NavBar;
