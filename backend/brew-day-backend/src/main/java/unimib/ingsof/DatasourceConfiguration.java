92d940f92879498a2087aa344a9ff1c658fa8bd4
frefolli <f.refolli@campus.unimib.it>
2023-01-22 22:34:37 +0100

    add database related dependency

---
+++ backend/brew-day-backend/src/main/java/unimib/ingsof/DatasourceConfiguration.java
@@ -0,0 +1,18 @@
+package unimib.ingsof;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatasourceConfiguration {
+    @Bean
+    public DataSource getDataSource() {
+        return DataSourceBuilder.create()
+          .driverClassName("org.sqlite.JDBC")
+          .url("jdbc:sqlite::memory:")
+          .build();	
+    }
+}
