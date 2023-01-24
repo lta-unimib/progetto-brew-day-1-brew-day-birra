a0495c4cd861f657fb6b9a7173ce9928fd27b50f
qazimtoska <q.toska@campus.unimib.it>
2023-01-24 23:13:49 +0100

    QuantityInput test added

--- frontend/brew-day/src/tests/Components.test.js
+++ frontend/brew-day/src/tests/Components.test.js
@@ -3,6 +3,7 @@ import { render } from '@testing-library/react';
 import Button from '../components/Button';
 import GridInventoryItem from '../components/GridInventoryItem';
 import NavBar from '../components/NavBar';
+import QuantityInput from '../components/QuantityInput';
 
 describe('Button component', () => {
     test('should render correctly', () => {
@@ -21,6 +22,13 @@ describe('GridInventoryItem component', () => {
 describe('NavBar component', () => {
     test('should render correctly', () => {
       const { container } = render(<NavBar />);
+      expect(container.firstChild).toMatchSnapshot();
+    });
+});
+
+describe('QuantityInput component', () => {
+    test('should render correctly', () => {
+      const { container } = render(<QuantityInput />);
       expect(container.firstChild).toMatchSnapshot();
     });
 });
