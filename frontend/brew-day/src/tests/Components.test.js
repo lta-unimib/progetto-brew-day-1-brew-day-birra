2d6f6b71c1f3476d1d0f05cc28d24e81b5078906
qazimtoska <q.toska@campus.unimib.it>
2023-01-24 23:12:12 +0100

    NavBar test added

--- frontend/brew-day/src/tests/Components.test.js
+++ frontend/brew-day/src/tests/Components.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from '@testing-library/react';
 import Button from '../components/Button';
 import GridInventoryItem from '../components/GridInventoryItem';
+import NavBar from '../components/NavBar';
 
 describe('Button component', () => {
     test('should render correctly', () => {
@@ -10,9 +11,16 @@ describe('Button component', () => {
     });
   });
 
-  describe('GridInventoryItem component', () => {
+describe('GridInventoryItem component', () => {
     test('should render correctly', () => {
       const { container } = render(<GridInventoryItem />);
       expect(container.firstChild).toMatchSnapshot();
     });
-  });
+});
+
+describe('NavBar component', () => {
+    test('should render correctly', () => {
+      const { container } = render(<NavBar />);
+      expect(container.firstChild).toMatchSnapshot();
+    });
+});
