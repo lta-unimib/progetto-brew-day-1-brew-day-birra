482b36d9f9be0a807c601818e6fdda8816264dda
qazimtoska <q.toska@campus.unimib.it>
2023-01-24 23:11:06 +0100

    GridInventoryItem test added

--- frontend/brew-day/src/tests/Components.test.js
+++ frontend/brew-day/src/tests/Components.test.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { render } from '@testing-library/react';
 import Button from '../components/Button';
+import GridInventoryItem from '../components/GridInventoryItem';
 
 describe('Button component', () => {
     test('should render correctly', () => {
       const { container } = render(<Button />);
+      expect(container.firstChild).toMatchSnapshot();
+    });
+  });
+
+  describe('GridInventoryItem component', () => {
+    test('should render correctly', () => {
+      const { container } = render(<GridInventoryItem />);
       expect(container.firstChild).toMatchSnapshot();
     });
   });
