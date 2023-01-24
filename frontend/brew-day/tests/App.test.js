94116e1dd0451c56ec9e9ac4bd42899ee5a1f55f
qazimtoska <q.toska@campus.unimib.it>
2023-01-25 00:35:35 +0100

    index.js is test covered

--- frontend/brew-day/tests/App.test.js
+++ frontend/brew-day/tests/App.test.js
@@ -1,10 +1,41 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { render } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import { create } from 'react-test-renderer';
+import '@testing-library/jest-dom/extend-expect';
 import App from '../src/App';
 
 describe('App component', () => {
   test('should render correctly', () => {
     const { container } = render(<App />);
     expect(container.firstChild).toMatchSnapshot();
+  });
+});
+
+let container;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
+
+describe('index.js', () => {
+  test('should render the App component', () => {
+    act(() => {
+      ReactDOM.render(
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>,
+        container
+      );
+    });
+    const component = create(<App />);
+    expect(component.toJSON()).toMatchSnapshot();
   });
 });
