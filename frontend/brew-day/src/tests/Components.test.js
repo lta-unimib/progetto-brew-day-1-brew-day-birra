a6962390269bb0e4c51b49d093bdd9fc34175daa
qazimtoska <q.toska@campus.unimib.it>
2023-01-24 23:48:23 +0100

    Button component is fully tested

--- frontend/brew-day/src/tests/Components.test.js
+++ frontend/brew-day/src/tests/Components.test.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent } from '@testing-library/react';
 import Button from '../components/Button';
 import GridInventoryItem from '../components/GridInventoryItem';
 import NavBar from '../components/NavBar';
@@ -9,6 +10,33 @@ describe('Button component', () => {
     test('should render correctly', () => {
       const { container } = render(<Button />);
       expect(container.firstChild).toMatchSnapshot();
+    });
+
+    test('should increase the count when "+" button is clicked', () => {
+      const { getByText } = render(<Button />);
+      const plusButton = getByText('+');
+      fireEvent.click(plusButton);
+      const countText = getByText('1');
+      expect(countText).toBeInTheDocument();
+    });
+
+    test('should decrease the count when "-" button is clicked if count is greater than zero', () => {
+      const { getByText } = render(<Button />);
+      const plusButton = getByText('+');
+      fireEvent.click(plusButton);
+      fireEvent.click(plusButton);
+      const minusButton = getByText('-');
+      fireEvent.click(minusButton);
+      const countText = getByText('1');
+      expect(countText).toBeInTheDocument();
+    });
+
+    test('should not decrease the count when "-" button is clicked if count is zero', () => {
+      const { getByText } = render(<Button />);
+      const minusButton = getByText('-');
+      fireEvent.click(minusButton);
+      const countText = getByText('0');
+      expect(countText).toBeInTheDocument();
     });
   });
 
