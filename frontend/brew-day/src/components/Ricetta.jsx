1f613d475626cffd23cbee9f625643781a961ab7
qazimtoska <q.toska@campus.unimib.it>
2023-01-25 17:29:18 +0100

    Basic structure of Ricetta, but still not hooked to Ricette page

--- frontend/brew-day/src/components/Ricetta.jsx
+++ frontend/brew-day/src/components/Ricetta.jsx
@@ -1,10 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Ricetta = () => {
-    return (
-        <div>
-        </div>
-    );
-}
+const Ricetta = ({ match }) => {
+  const recipeId = match.params.id;
+  const [recipe, setRecipe] = useState({ name: "", description: "" });
+
+  return (
+    <div>
+      <label>Nome:</label>
+      <input type="text" value={recipe.name} onChange={handleNameChange} />
+      <br />
+      <label>Descrizione:</label>
+      <textarea value={recipe.description} onChange={handleDescriptionChange} />
+    </div>
+  );
+};
 
 export default Ricetta;
