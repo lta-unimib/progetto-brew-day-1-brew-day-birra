9a360e59563c77d40739eae8163d344c8076eee9
qazimtoska <q.toska@campus.unimib.it>
2023-01-26 12:48:59 +0100

    Added tests to front-end so coverage is above 87%

--- frontend/brew-day/tests/Pages.test.js
+++ frontend/brew-day/tests/Pages.test.js
@@ -1,5 +1,6 @@
+import '@testing-library/jest-dom/extend-expect';
 import React from "react";
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Birre from '../src/pages/Birre';
 import Home from '../src/pages/Home';
 import Inventario from '../src/pages/Inventario';
@@ -31,6 +32,33 @@ describe('Ricette component', () => {
     test('should render correctly', () => {
       const { container } = render(<Ricette />);
       expect(container.firstChild).toMatchSnapshot();
+    });
+
+    test("handleView shows modal with correct content", () => {
+      const { getAllByText } = render(<Ricette />);
+
+      const viewButtons = getAllByText("Visualizza");
+      fireEvent.click(viewButtons[0]);
+    
+      expect(viewButtons[0]).toBeInTheDocument();
+    });
+
+    test("handleEdit shows modal with correct content", () => {
+      const { getAllByText } = render(<Ricette />);
+    
+      const viewButtons = getAllByText("Modifica");
+      fireEvent.click(viewButtons[0]);
+
+      expect(viewButtons[0]).toBeInTheDocument();
+    });
+
+    test("handleDelete shows modal with correct content", () => {
+      const { getAllByText } = render(<Ricette />);
+    
+      const viewButtons = getAllByText("Rimuovi");
+      fireEvent.click(viewButtons[0]);
+
+      expect(viewButtons[0]).toBeInTheDocument();
     });
 });
 
