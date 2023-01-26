748f1362732807f811de3e883d4f9d11a28b13cb
qazimtoska <q.toska@campus.unimib.it>
2023-01-26 17:16:32 +0100

    Now you can edit recipes
    Ricette has been converted to a class

--- frontend/brew-day/src/components/RecipeEdit.jsx
+++ frontend/brew-day/src/components/RecipeEdit.jsx
@@ -1,14 +1,38 @@
 import React from "react";
 
-const RecipeEdit = (props) => {
-  return (
-    <div>
-      <p>Nome:</p>
-      <textarea id="nameTextArea" value={props.name}></textarea>
-      <p>Descrizione:</p>
-      <textarea id="descriptionTextArea" value={props.description}></textarea>
-    </div>
-  );
-};
+class RecipeEdit extends React.Component {
+    constructor(props) {
+        super(props);
+        this.nameTextChanged = this.nameTextChanged.bind(this);
+        this.descriptionTextChanged = this.descriptionTextChanged.bind(this);
+        this.state = {
+            name: props.name,
+            description: props.description
+        };
+    }
+
+    nameTextChanged(event) {
+        this.setState({
+            name: event.target.value
+        });
+    }
+
+    descriptionTextChanged(event) {
+        this.setState({
+            description: event.target.value
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Nome:</p>
+                <textarea id="nameTextArea" value={this.state.name} onChange={this.nameTextChanged}></textarea>
+                <p>Descrizione:</p>
+                <textarea id="descriptionTextArea" value={this.state.description} onChange={this.descriptionTextChanged}></textarea>
+            </div>
+        );
+    }
+}
 
 export default RecipeEdit;
