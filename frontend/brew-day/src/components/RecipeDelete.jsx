fcdff66d3f75b119e1a7766a3b6dd376f6aa03e5
qazimtoska <q.toska@campus.unimib.it>
2023-01-26 19:24:44 +0100

    Rimuovi layout produces a message when deleting the recipe

--- frontend/brew-day/src/components/RecipeDelete.jsx
+++ frontend/brew-day/src/components/RecipeDelete.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import RecipeView from "./RecipeView";
 
 const RecipeDelete = (props) => {
+  const [showRecipe, setShowRecipe] = useState(true);
+  const [showButton, setShowButton] = useState(true);
+
+  const handleClick = () => {
+    setShowRecipe(false);
+    setShowButton(false);
+  };
+
   return (
     <div>
-      <RecipeView name={props.name} description={props.description} ingredients={props.ingredients}/>
-      <button className="recipeButton">Sei sicuro di voler rimuovere la ricetta?</button>
+      {showRecipe ? (
+        <RecipeView name={props.name} description={props.description} ingredients={props.ingredients}/>
+      ) : (
+        <p>Ricetta rimossa correttamente</p>
+      )}
+      {showButton && (
+        <button className="recipeButton" onClick={handleClick}>Sei sicuro di voler rimuovere la ricetta?</button>
+      )}
     </div>
   );
 };
