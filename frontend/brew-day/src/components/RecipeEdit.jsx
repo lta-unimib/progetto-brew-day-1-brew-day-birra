a6017110264ecc8b67081fcb93265731d62016d1
qazimtoska <q.toska@campus.unimib.it>
2023-01-26 22:06:53 +0100

    RecipeEdit is richer

--- frontend/brew-day/src/components/RecipeEdit.jsx
+++ frontend/brew-day/src/components/RecipeEdit.jsx
@@ -1,38 +1,109 @@
 import React from "react";
 
 class RecipeEdit extends React.Component {
-    constructor(props) {
-        super(props);
-        this.nameTextChanged = this.nameTextChanged.bind(this);
-        this.descriptionTextChanged = this.descriptionTextChanged.bind(this);
-        this.state = {
-            name: props.name,
-            description: props.description
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: props.name,
+      description: props.description,
+      buttonClicked: false,
+    };
+  }
 
-    nameTextChanged(event) {
-        this.setState({
-            name: event.target.value
-        });
-    }
-
-    descriptionTextChanged(event) {
-        this.setState({
-            description: event.target.value
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <p>Nome:</p>
-                <textarea id="nameTextArea" value={this.state.name} onChange={this.nameTextChanged}></textarea>
-                <p>Descrizione:</p>
-                <textarea id="descriptionTextArea" value={this.state.description} onChange={this.descriptionTextChanged}></textarea>
+  render() {
+    return (
+      <div>
+        {!this.state.buttonClicked ? (
+          <div>
+            <div id="recipeEditDiv">
+              <p>Nome:</p>
+              <textarea
+                id="nameTextArea"
+                value={this.state.name}
+                onChange={this.nameTextChanged}
+              ></textarea>
+              <p>Descrizione:</p>
+              <textarea
+                id="descriptionTextArea"
+                value={this.state.description}
+                onChange={this.descriptionTextChanged}
+              ></textarea>
+              <br></br>
+              <table className="recipeView">
+                <thead>
+                  <tr>
+                    <th>Ingrediente</th>
+                    <th>Quantità</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <p>Acqua</p>
+                    </td>
+                    <td>
+                      <textarea></textarea>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <p>Additivi</p>
+                    </td>
+                    <td>
+                      <textarea></textarea>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <p>Lievito</p>
+                    </td>
+                    <td>
+                      <textarea></textarea>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <p>Luppoli</p>
+                    </td>
+                    <td>
+                      <textarea></textarea>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <p>Malto</p>
+                    </td>
+                    <td>
+                      <textarea></textarea>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <p>Zuccheri</p>
+                    </td>
+                    <td>
+                      <textarea></textarea>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <button
+                onClick={() => {
+                  this.setState({
+                    buttonClicked: true,
+                  });
+                }}
+              >
+                Modifica ricetta
+              </button>
             </div>
-        );
-    }
+          </div>
+        ) : (
+          <p>Ricetta modificata correttamente</p>
+        )}
+      </div>
+    );
+  }
 }
 
 export default RecipeEdit;
--- frontend/brew-day/src/components/RecipeView.jsx
+++ frontend/brew-day/src/components/RecipeView.jsx
@@ -5,7 +5,7 @@ const RecipeView = (props) => {
     <div>
       <h1>{props.name}</h1>
       <p>{props.description}</p>
-      <table id="recipeView">
+      <table className="recipeView">
         <thead>
           <tr>
             <th>Nome</th>
