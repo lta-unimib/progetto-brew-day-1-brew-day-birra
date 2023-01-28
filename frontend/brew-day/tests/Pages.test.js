216878ec447ee9f721fff03246df19e912dba01d
qazimtoska <q.toska@campus.unimib.it>
2023-01-28 18:51:43 +0100

    First test of a copmonent that uses fetch that works

--- frontend/brew-day/tests/Pages.test.js
+++ frontend/brew-day/tests/Pages.test.js
@@ -1,9 +1,25 @@
 import "@testing-library/jest-dom/extend-expect";
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Birre from "../src/pages/Birre";
 import Home from "../src/pages/Home";
 import Spesa from "../src/pages/Spesa";
+import Inventario from "../src/pages/Inventario";
+
+global.fetch = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+        json: () => Promise.resolve([{name: "ingredient1", quantity: 2.0}, {name: "ingredient2", quantity: 3.0}]),
+    })
+);
+
+describe("Inventario", () => {
+    it("loads the inventory on mount", async () => {
+        render(<Inventario />);
+        await screen.findByText("ingredient1", {exact: false});
+        expect(screen.getByText("ingredient1")).toBeInTheDocument();
+        expect(screen.getByText("ingredient2")).toBeInTheDocument();
+    });
+});
 
 describe("Birre component", () => {
   test("should render correctly", () => {
