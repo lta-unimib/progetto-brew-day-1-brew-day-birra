27a2fd8da83bd03b775bf704b650d27bffbca6bb
qazimtoska <q.toska@campus.unimib.it>
2023-01-29 00:28:22 +0100

    Some tests added, code smell removed

--- frontend/brew-day/tests/Pages.test.js
+++ frontend/brew-day/tests/Pages.test.js
@@ -1,24 +1,45 @@
 import "@testing-library/jest-dom/extend-expect";
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import Birre from "../src/pages/Birre";
 import Home from "../src/pages/Home";
 import Spesa from "../src/pages/Spesa";
 import Inventario from "../src/pages/Inventario";
 
 global.fetch = jest.fn().mockImplementation(() =>
-    Promise.resolve({
-        json: () => Promise.resolve([{name: "ingredient1", quantity: 2.0}, {name: "ingredient2", quantity: 3.0}]),
-    })
+  Promise.resolve({
+    json: () =>
+      Promise.resolve([
+        { name: "ingredient1", quantity: 2.0 },
+        { name: "ingredient2", quantity: 3.0 },
+      ]),
+  })
 );
 
-describe("Inventario", () => {
-    it("loads the inventory on mount", async () => {
-        render(<Inventario />);
-        await screen.findByText("ingredient1", {exact: false});
-        expect(screen.getByText("ingredient1")).toBeInTheDocument();
-        expect(screen.getByText("ingredient2")).toBeInTheDocument();
+describe("Inventario component", () => {
+  test("loads the inventory on mount", async () => {
+    render(<Inventario />);
+    await screen.findByText("ingredient1", { exact: false });
+    expect(screen.getByText("ingredient1")).toBeInTheDocument();
+    expect(screen.getByText("ingredient2")).toBeInTheDocument();
+  });
+
+  test("renders the inventory items", async () => {
+    render(<Inventario />);
+    await screen.findByText("ingredient1", { exact: false });
+    expect(screen.getAllByText("ingredient1")).toHaveLength(1);
+    expect(screen.getAllByText("ingredient2")).toHaveLength(1);
+  });
+
+  test("deletes an item from the inventory and updates the state", async () => {
+    render(<Inventario />);
+    await waitFor(() => screen.getAllByText("Elimina ingrediente"));
+    const deleteButton = screen.getAllByText("Elimina ingrediente")[0];
+    fireEvent.click(deleteButton);
+    await waitFor(() => {
+      expect(screen.queryByText("ingredient1")).not.toBeInTheDocument();
     });
+  });
 });
 
 describe("Birre component", () => {
