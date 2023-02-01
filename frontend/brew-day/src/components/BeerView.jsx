9a55d83a46922b9532faca5359344c210403ece3
qazimtoska <q.toska@campus.unimib.it>
2023-02-01 19:09:20 +0100

    Now you can see the recipe of a beer in 'Dettagli'

--- frontend/brew-day/src/components/BeerView.jsx
+++ frontend/brew-day/src/components/BeerView.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import RecipeView from "./RecipeView";
 
 const BeerView = (props) => {
-  return (
-    <div>
-      <center>
-        <h1>{props.name}</h1>
-        <h4>ID ricetta: {props.recipeID}</h4>
-        <h4>Note:</h4>
-        {props.notes.map((note) => (
-          <p key={note.id}>• {note.description}</p>
-        ))}
-      </center>
-    </div>
-  );
-};
+    const [showRicetta, setShowRicetta] = useState(false);
+  
+    return (
+      <div>
+        <center>
+          <h1>{props.name}</h1>
+          <button onClick={() => setShowRicetta(true)}>Visualizza ricetta</button>
+          {showRicetta && <RecipeView name="Prova" description="Prova" ingredients={[]}/>}
+          <h4>Note:</h4>
+          {props.notes.map((note) => (
+            <p key={note.id}>• {note.description}</p>
+          ))}
+        </center>
+      </div>
+    );
+  };
+  
+  export default BeerView;
 
-export default BeerView;
+//<h4>ID ricetta: {props.recipeID}</h4>
