b87b0e7548e7b115e94afe2057878217b2ff8eb6
frefolli <f.refolli@campus.unimib.it>
2023-02-03 18:37:30 +0100

    fix code smells

--- backend/brew-day-backend/src/test/java/unimib/ingsof/persistence/model/SettingTest.java
+++ backend/brew-day-backend/src/test/java/unimib/ingsof/persistence/model/SettingTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class SettingTest {
+class SettingTest {
 	@Test
 	void testBehavior() {
 		Setting recipe = new Setting("settingID", "settingValue");
