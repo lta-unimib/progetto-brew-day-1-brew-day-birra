3feb2e6c28b4c0d66a39310f84f36b7422d885d2
qazimtoska <q.toska@campus.unimib.it>
2023-02-03 22:33:11 +0100

    Added tests - locally 86%

--- frontend/brew-day/tests/BirreActions.test.js
+++ frontend/brew-day/tests/BirreActions.test.js
@@ -1,14 +1,34 @@
 import React from "react";
 import { render, screen, waitFor, fireEvent } from "@testing-library/react";
 import Birre from "../src/pages/Birre";
+import BeerEdit from "../src/components/BeerEdit";
 import { existsSync, readFileSync, writeFileSync } from "fs";
-import { promisify } from 'util';
 
 const log = (string) => {
   if (!existsSync("./log.log")) writeFileSync("./log.log", "");
   let text = readFileSync("./log.log").toString();
   text += "\n" + string;
   writeFileSync("./log.log", text);
+};
+
+const testBeer = {
+  beerID: "1",
+  name: "Beer 1",
+  recipeID: "recipeID",
+  notes: [
+    {
+      beerID: "1",
+      noteID: "noteID1",
+      noteType: "generic",
+      description: "Description 1",
+    },
+    {
+      beerID: "1",
+      noteID: "noteID3",
+      noteType: "taste",
+      description: "Description 3",
+    },
+  ],
 };
 
 global.fetch = jest.fn().mockImplementation((url) => {
@@ -22,7 +42,7 @@ global.fetch = jest.fn().mockImplementation((url) => {
           notes: [
             {
               beerID: "1",
-              noteID: "noteID",
+              noteID: "noteID1",
               noteType: "generic",
               description: "Description 1",
             },
@@ -39,7 +59,7 @@ global.fetch = jest.fn().mockImplementation((url) => {
           notes: [
             {
               beerID: "2",
-              noteID: "noteID",
+              noteID: "noteID2",
               noteType: "generic",
               description: "Description 2",
             },
@@ -81,7 +101,9 @@ describe("Birre component", () => {
     await waitFor(() => screen.getAllByText("Elimina")[0]);
     const deleteButton = screen.getAllByText("Elimina")[0];
     fireEvent.click(deleteButton);
-    await waitFor(() => screen.getByText("Sei sicuro di voler eliminare questa birra?"));
+    await waitFor(() =>
+      screen.getByText("Sei sicuro di voler eliminare questa birra?")
+    );
     expect(screen.getByText("Sei sicuro di voler eliminare questa birra?"));
   });
 
@@ -94,5 +116,44 @@ describe("Birre component", () => {
     const confirmDeleteButton = screen.getAllByText("Elimina")[2];
     fireEvent.click(confirmDeleteButton);
     expect(screen.queryByText("Beer 2")).toBeNull();
+  });
+
+  test("deletes note", async () => {
+    render(
+      <BeerEdit beerID={testBeer.beerID} name={testBeer.name} notes={testBeer.notes} />
+    );
+    await waitFor(() => screen.getAllByText("Elimina nota")[0]);
+    const deleteButton = screen.getAllByText("Elimina nota")[0];
+    fireEvent.click(deleteButton);
+    expect(screen.queryByText("Description 2")).toBeNull();
+  });
+
+  test("deletes note", async () => {
+    render(
+      <BeerEdit beerID={testBeer.beerID} name={testBeer.name} notes={testBeer.notes} />
+    );
+    await waitFor(() => screen.getAllByText("Elimina nota")[0]);
+    const deleteButton = screen.getAllByText("Elimina nota")[0];
+    fireEvent.click(deleteButton);
+    expect(screen.queryByText("generic")).toBeNull();
+  });
+
+  test("add note", async () => {
+    render(
+      <BeerEdit beerID={testBeer.beerID} name={testBeer.name} notes={testBeer.notes} />
+    );
+    await waitFor(() => screen.getAllByText("Aggiungi nota")[0]);
+    const addButton = screen.getAllByText("Aggiungi nota")[0];
+    const descriptionTextArea = screen.getAllByTestId("description-textarea")[0];
+    const noteTypeTextArea = screen.getAllByTestId("note-type-textarea")[0];
+    fireEvent.change(descriptionTextArea, { target: { value: 'test description' }});
+    fireEvent.change(noteTypeTextArea, { target: { value: 'generic' }});
+    fireEvent.click(addButton);
+    render(<Birre />);
+    await waitFor(() => screen.getAllByText("Modifica")[0]);
+    const editButton = screen.getAllByText("Modifica")[0];
+    fireEvent.click(editButton);
+    await waitFor(() => screen.getByText("test description"));
+    expect(screen.getByText("test description"));
   });
 });
