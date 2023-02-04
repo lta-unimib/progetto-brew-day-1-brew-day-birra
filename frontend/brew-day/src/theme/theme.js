2556488ff1ba7a8883de10c7ae44f78a8d0b45b4
qazimtoska <q.toska@campus.unimib.it>
2023-02-04 11:33:30 +0100

    Button in Spesa follows MUI guidelines

--- frontend/brew-day/src/theme/theme.js
+++ frontend/brew-day/src/theme/theme.js
@@ -3,7 +3,8 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#FFDE03',
+      main: '#FFC107',
+      contrastText: "#fff",
     },
   },
 });
