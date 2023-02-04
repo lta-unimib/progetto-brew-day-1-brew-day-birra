2543631f3d023289b74d738f437a79b4b703693c
qazimtoska <q.toska@campus.unimib.it>
2023-02-04 11:27:07 +0100

    Yellow theme applied

--- frontend/brew-day/src/pages/Spesa.jsx
+++ frontend/brew-day/src/pages/Spesa.jsx
@@ -1,56 +1,97 @@
 import React from "react";
 import QuantityInput from "../components/QuantityInput";
-import { Button } from '@mui/material';
+import { Button, ThemeProvider } from "@mui/material";
+import theme from "../theme/theme";
 
 const Spesa = () => {
-    return (
-      <center>
-        <div>
-          <table className="myTable">
-            <tr>
-              <th>Foto prodotto</th>
-              <th>Nome - Descrizione</th>
-              <th>Quantità</th>
-            </tr>
-            <tr>
-              <td><img className="shoppingImage" src="../../icons/inventory-icons/acqua.png"/></td>
-              <td>Acqua</td>
-              <td><QuantityInput /></td>
-            </tr>
-            <tr>
-              <td><img className="shoppingImage" src="../../icons/inventory-icons/additivi.png"/></td>
-              <td>Additivi</td>
-              <td><QuantityInput /></td>
-            </tr>
-            <tr>
-              <td><img className="shoppingImage" src="../../icons/inventory-icons/lievito.png"/></td>
-              <td>Lievito</td>
-              <td><QuantityInput /></td>
-            </tr>
-            <tr>
-              <td><img className="shoppingImage" src="../../icons/inventory-icons/luppoli.png"/></td>
-              <td>Luppoli</td>
-              <td><QuantityInput /></td>
-            </tr>
-            <tr>
-              <td><img className="shoppingImage" src="../../icons/inventory-icons/malto.png"/></td>
-              <td>Malto</td>
-              <td><QuantityInput /></td>
-            </tr>
-            <tr>
-              <td><img className="shoppingImage" src="../../icons/inventory-icons/zuccheri.png"/></td>
-              <td>Zuccheri</td>
-              <td><QuantityInput /></td>
-            </tr>
-          </table>
-          <Button id="shoppingBuyButton" variant="contained" color="primary">
-            Compra
-          </Button>
-        </div>
-      </center>
-    );
-}
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        <table className="myTable">
+          <tr>
+            <th>Foto prodotto</th>
+            <th>Nome - Descrizione</th>
+            <th>Quantità</th>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="shoppingImage"
+                src="../../icons/inventory-icons/acqua.png"
+              />
+            </td>
+            <td>Acqua</td>
+            <td>
+              <QuantityInput />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="shoppingImage"
+                src="../../icons/inventory-icons/additivi.png"
+              />
+            </td>
+            <td>Additivi</td>
+            <td>
+              <QuantityInput />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="shoppingImage"
+                src="../../icons/inventory-icons/lievito.png"
+              />
+            </td>
+            <td>Lievito</td>
+            <td>
+              <QuantityInput />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="shoppingImage"
+                src="../../icons/inventory-icons/luppoli.png"
+              />
+            </td>
+            <td>Luppoli</td>
+            <td>
+              <QuantityInput />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="shoppingImage"
+                src="../../icons/inventory-icons/malto.png"
+              />
+            </td>
+            <td>Malto</td>
+            <td>
+              <QuantityInput />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="shoppingImage"
+                src="../../icons/inventory-icons/zuccheri.png"
+              />
+            </td>
+            <td>Zuccheri</td>
+            <td>
+              <QuantityInput />
+            </td>
+          </tr>
+        </table>
+        <Button id="shoppingBuyButton" variant="contained" color="primary">
+          Compra
+        </Button>
+      </div>
+    </ThemeProvider>
+  );
+};
 
 export default Spesa;
-
-//<button id="shoppingBuyButton">Compra</button>
