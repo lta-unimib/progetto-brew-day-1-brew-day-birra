0722875488f3cba09805fdc83433ae296e269d61
qazimtoska <q.toska@campus.unimib.it>
2023-02-06 17:18:53 +0100

    Test refactor

--- frontend/brew-day/tests/beer-tests/RenderBeerPage.test.js
+++ frontend/brew-day/tests/beer-tests/RenderBeerPage.test.js
@@ -1,40 +1,45 @@
 import React from "react";
-import { render, screen, act, waitFor, fireEvent } from "@testing-library/react";
+import {
+  render,
+  screen,
+  act,
+  waitFor,
+  fireEvent,
+} from "@testing-library/react";
 import Birre from "../../src/pages/Birre";
 
 global.fetch = jest.fn().mockImplementation((url) => {
   if (url.endsWith("/api/beer")) {
     return Promise.resolve({
-      json: () =>
-        Promise.resolve(["beer1"]),
+      json: () => Promise.resolve(["beer1"]),
     });
   } else if (url.endsWith("/api/beer/beer1")) {
     return Promise.resolve({
-        json: () =>
-          Promise.resolve({
-            beerID: "beer1",
-            name: "Beer 1",
-            recipeID: "recipe1",
-            notes: [
-              {
-                beerID: "beer1",
-                noteID: "note1",
-                noteType: "generic",
-                description: "Beer 1's description",
-              },
-            ],
-          }),
-      });
+      json: () =>
+        Promise.resolve({
+          beerID: "beer1",
+          name: "Beer 1",
+          recipeID: "recipe1",
+          notes: [
+            {
+              beerID: "beer1",
+              noteID: "note1",
+              noteType: "generic",
+              description: "Beer 1's description",
+            },
+          ],
+        }),
+    });
   } else if (url.endsWith("/api/recipes/recipe1")) {
     return Promise.resolve({
-        json: () =>
-          Promise.resolve({
-            recipeID: "recipe1",
-            name: "ricetta1",
-            description: "Recipe 1's description ",
-            ingredients: []
+      json: () =>
+        Promise.resolve({
+          recipeID: "recipe1",
+          name: "ricetta1",
+          description: "Recipe 1's description",
+          ingredients: [],
         }),
-      });
+    });
   }
 });
 
@@ -44,10 +49,19 @@ test("renders 'Birre' page", async () => {
 });
 
 test("renders beer details", async () => {
-    render(<Birre />);
-    await waitFor(() => screen.getAllByText("Dettagli")[0]);
-    const detailsButton = screen.getAllByText("Dettagli")[0];
-    fireEvent.click(detailsButton);
-    await waitFor(() => screen.getByText("• Beer 1's description"));
-    expect(screen.getByText("• Beer 1's description"));
-  });
+  render(<Birre />);
+  await waitFor(() => screen.getAllByText("Dettagli")[0]);
+  const detailsButton = screen.getAllByText("Dettagli")[0];
+  fireEvent.click(detailsButton);
+  await waitFor(() => screen.getByText("• Beer 1's description"));
+  expect(screen.getByText("• Beer 1's description"));
+});
+
+test("renders beer edit", async () => {
+  render(<Birre />);
+  await waitFor(() => screen.getAllByText("Modifica")[0]);
+  const editButton = screen.getAllByText("Modifica")[0];
+  fireEvent.click(editButton);
+  await waitFor(() => screen.getByText("Beer 1's description"));
+  expect(screen.getByText("Beer 1's description"));
+});
