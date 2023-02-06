077f6068253ec4b939789313ca9b7df7bef581ae
qazimtoska <q.toska@campus.unimib.it>
2023-02-06 01:07:47 +0100

    Added if-else to render the correct MButton

--- frontend/brew-day/src/components/MButton.jsx
+++ frontend/brew-day/src/components/MButton.jsx
@@ -1,23 +1,34 @@
 import React, { Component } from "react";
-import { Button } from '@mui/material';
+import { Button } from "@mui/material";
 
 class MButton extends Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return (
-      <div className={this.props.className ?? ''} id={this.props.id ?? ''}>
+    const button = (
       <Button
-        style={{ marginRight: 10, marginLeft: 10, marginTop: 10, marginBottom: 10 }}
+        style={{
+          marginRight: 10,
+          marginLeft: 10,
+          marginTop: 10,
+          marginBottom: 10,
+        }}
         variant="contained"
         color="primary"
         onClick={this.props.onClick ?? (() => {})}
       >
         {this.props.text}
       </Button>
-      </div>
     );
+    if(this.props.id || this.props.className) {
+      return (
+        <div className={this.props.className ?? ""} id={this.props.id ?? ""}>
+          {button}
+        </div>
+      );
+    }
+    return (button);
   }
 }
 
