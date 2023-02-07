071d36a3ab26c9a58f2f88ef73bf9fcd792f4b68
alegil0206 <a.gilardi7@campus.unimib.it>
2023-02-07 22:41:16 +0100

    Spostato postShoppingList in DoShoppingEndpoint

--- backend/brew-day-backend/src/main/java/unimib/ingsof/api/ShoppingEndpoint.java
+++ backend/brew-day-backend/src/main/java/unimib/ingsof/api/ShoppingEndpoint.java
@@ -10,14 +10,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import unimib.ingsof.exceptions.DoesntExistsException;
 import unimib.ingsof.exceptions.InsufficientEquipmentException;
-import unimib.ingsof.exceptions.ValidationException;
 import unimib.ingsof.logic.ShoppingController;
 import unimib.ingsof.persistence.view.IngredientView;
 
@@ -41,16 +39,5 @@ public class ShoppingEndpoint {
 		}
 		return new ResponseEntity<>(result, HttpStatus.OK);
     }
-	
-	@PostMapping
-    public ResponseEntity<Object> postShoppingList(@RequestBody List<Map<String, String>> ingredients) {
-		try {
-			shoppingController.postShoppingList(ingredients);
-			return new ResponseEntity<>(HttpStatus.OK);
-		} catch (ValidationException e) {
-			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		} catch (Exception e) {
-			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-    }
+
 }
