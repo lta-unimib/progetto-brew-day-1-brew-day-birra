51610aec6dc5f66f8a667df29f4da51b7691e983
alegil0206 <a.gilardi7@campus.unimib.it>
2023-02-07 20:07:33 +0100

    Update BeerListController.java

--- backend/brew-day-backend/src/main/java/unimib/ingsof/logic/BeerListController.java
+++ backend/brew-day-backend/src/main/java/unimib/ingsof/logic/BeerListController.java
@@ -7,10 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import unimib.ingsof.exceptions.DoesntExistsException;
-import unimib.ingsof.exceptions.NotEnoughIngredientsException;
-import unimib.ingsof.exceptions.ValidationException;
-import unimib.ingsof.exceptions.WrongIDGenerationInitialization;
+import unimib.ingsof.exceptions.InsufficientEquipmentException;
 import unimib.ingsof.generation.id.IDGenerationFacade;
 import unimib.ingsof.persistence.repository.BeerRepository;
 import unimib.ingsof.persistence.service.Protocol;
@@ -24,27 +21,34 @@ public class BeerListController {
 	private RecipeController recipeController;
 	@Autowired
 	ExecuteRecipeController executeRecipeController;
+	@Autowired
+	private SettingController settingController;
 	
 	public List<String> getAllBeerIDs(Optional<String> filterByName, Optional<String> filterByRecipeID) {
 		return beerRepository.getAllBeerIDs(filterByName.orElse(""), filterByRecipeID.orElse(""));
 	}
 	
-	public String addBeer(Map<String, String> beerObject) throws ValidationException, WrongIDGenerationInitialization, DoesntExistsException, NotEnoughIngredientsException {
+	public String addBeer(Map<String, String> beerObject) throws Exception {
 		beerObject = BeerInitializationValidator.getInstance().handle(beerObject);
 		String name = beerObject.get(Protocol.NAME_KEY);
 		String recipeID = beerObject.get(Protocol.RECIPE_ID_KEY);
 		float quantity = Float.parseFloat(beerObject.get(Protocol.QUANTITY_KEY));
 		recipeController.getRecipeByID(recipeID);
-		
+		float equipment = 0;
+		try {
+			equipment = Float.parseFloat(settingController.getEquipment());
+		} catch (Exception e) {
+			throw new Exception();
+		}
+		if (quantity > equipment)
+			throw new InsufficientEquipmentException();
 		executeRecipeController.execute(recipeID, quantity);
-
 		String beerID = "";
 		while(true) {
 			beerID = IDGenerationFacade.getInstance().generateBeerID(beerObject);
 			if (!beerRepository.getAllBeerIDs("", "").contains(beerID))
 				break;
 		}
-		
 		beerRepository.addBeer(beerID, name, recipeID);
 		return beerID;
 	}
