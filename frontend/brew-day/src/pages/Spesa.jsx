4aa7b6f440b29b21d1371481c5cb53437f246681
qazimtoska <q.toska@campus.unimib.it>
2023-02-07 21:29:25 +0100

    'Spesa' is ok

--- frontend/brew-day/src/pages/Spesa.jsx
+++ frontend/brew-day/src/pages/Spesa.jsx
@@ -1,32 +1,48 @@
 import React, { Component } from "react";
 import { ThemeProvider } from "@mui/material";
 import theme from "../theme/theme";
-import MButton from '../components/MButton';
+import MButton from "../components/MButton";
 
 class Spesa extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      ingredients: [{
-        ingredientName: '',
-        ingredientQuantity: ''
-      }]
+      ingredients: [
+        {
+          ingredientName: "",
+          ingredientQuantity: "",
+        },
+      ],
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleAddIngredient = this.handleAddIngredient.bind(this);
   }
 
   handleSubmit() {
-    fetch('api/shopping/test', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(this.state.ingredients.map(({ ingredientName, ingredientQuantity }) => ({ name: ingredientName, quantity: ingredientQuantity })))
-    }).then(() => this.setState({ ingredients: [{ ingredientName: '', ingredientQuantity: '' }] }));
+    fetch("api/shopping/test", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(
+        this.state.ingredients.map(
+          ({ ingredientName, ingredientQuantity }) => ({
+            name: ingredientName,
+            quantity: ingredientQuantity,
+          })
+        )
+      ),
+    }).then(() =>
+      this.setState({
+        ingredients: [{ ingredientName: "", ingredientQuantity: "" }],
+      })
+    );
   }
 
   handleAddIngredient() {
     this.setState({
-      ingredients: [...this.state.ingredients, { ingredientName: '', ingredientQuantity: '' }]
+      ingredients: [
+        ...this.state.ingredients,
+        { ingredientName: "", ingredientQuantity: "" },
+      ],
     });
   }
 
@@ -53,22 +69,36 @@ class Spesa extends Component {
                   <input
                     type="text"
                     value={ingredient.ingredientName}
-                    onChange={e => this.handleIngredientChange('ingredientName', index, e.target.value)}
+                    onChange={(e) =>
+                      this.handleIngredientChange(
+                        "ingredientName",
+                        index,
+                        e.target.value
+                      )
+                    }
                   />
                 </td>
                 <td>
                   <input
                     type="text"
                     value={ingredient.ingredientQuantity}
-                    onChange={e => this.handleIngredientChange('ingredientQuantity', index, e.target.value)}
+                    onChange={(e) =>
+                      this.handleIngredientChange(
+                        "ingredientQuantity",
+                        index,
+                        e.target.value
+                      )
+                    }
                   />
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
-        <MButton text="Aggiungi" id="shoppingBuyButton" onClick={this.handleSubmit} />
-        <MButton text="Altro ingrediente" onClick={this.handleAddIngredient} />
+        <div id="shoppingButtonContainer">
+          <MButton text="Altro ingrediente" onClick={this.handleAddIngredient} />
+          <MButton text="Aggiungi" onClick={this.handleSubmit} />
+        </div>
       </ThemeProvider>
     );
   }
