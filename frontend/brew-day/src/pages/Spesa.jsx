7ac3fc7f5c7f9429ba8f2aced781be1239e362dd
qazimtoska <q.toska@campus.unimib.it>
2023-02-07 19:32:30 +0100

    First version of 'Spesa' page
    No confirmation

--- frontend/brew-day/src/pages/Spesa.jsx
+++ frontend/brew-day/src/pages/Spesa.jsx
@@ -1,106 +1,60 @@
-import React from "react";
-import QuantityInput from "../components/QuantityInput";
+import React, { Component } from "react";
 import { ThemeProvider } from "@mui/material";
 import theme from "../theme/theme";
 import MButton from '../components/MButton';
 
-const Spesa = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <div>
+class Spesa extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredientName: '',
+      ingredientQuantity: '',
+    };
+  }
+
+  handleSubmit() {
+    fetch('api/shopping/test', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify([{ name: this.state.ingredientName, quantity: this.state.ingredientQuantity }]),
+    })
+      .then(response => response.json())
+      .then(this.setState({ ingredientName: '', ingredientQuantity: '' }));
+  }
+
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
         <table className="myTable">
           <thead>
             <tr>
-              <th>Foto prodotto</th>
-              <th>Nome - Descrizione</th>
+              <th>Nome ingrediente</th>
               <th>Quantità</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <td>
-                <img
-                  className="shoppingImage"
-                  src="../../icons/inventory-icons/acqua.png"
-                  alt=""
+                <input
+                  type="text"
+                  value={this.state.ingredientName}
+                  onChange={e => this.setState({ ingredientName: e.target.value })}
                 />
               </td>
-              <td>Acqua</td>
               <td>
-                <QuantityInput />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  className="shoppingImage"
-                  src="../../icons/inventory-icons/additivi.png"
-                  alt=""
+                <input
+                  type="text"
+                  value={this.state.ingredientQuantity}
+                  onChange={e => this.setState({ ingredientQuantity: e.target.value })}
                 />
-              </td>
-              <td>Additivi</td>
-              <td>
-                <QuantityInput />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  className="shoppingImage"
-                  src="../../icons/inventory-icons/lievito.png"
-                  alt=""
-                />
-              </td>
-              <td>Lievito</td>
-              <td>
-                <QuantityInput />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  className="shoppingImage"
-                  src="../../icons/inventory-icons/luppoli.png"
-                  alt=""
-                />
-              </td>
-              <td>Luppoli</td>
-              <td>
-                <QuantityInput />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  className="shoppingImage"
-                  src="../../icons/inventory-icons/malto.png"
-                  alt=""
-                />
-              </td>
-              <td>Malto</td>
-              <td>
-                <QuantityInput />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  className="shoppingImage"
-                  src="../../icons/inventory-icons/zuccheri.png"
-                  alt=""
-                />
-              </td>
-              <td>Zuccheri</td>
-              <td>
-                <QuantityInput />
               </td>
             </tr>
           </tbody>
         </table>
-        <MButton text="Compra" id="shoppingBuyButton" />
-      </div>
-    </ThemeProvider>
-  );
-};
+        <MButton text="Aggiungi" id="shoppingBuyButton" onClick={() => this.handleSubmit()} />
+      </ThemeProvider>
+    );
+  }
+}
 
 export default Spesa;
