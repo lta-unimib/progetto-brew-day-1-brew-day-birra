249a8d62115f1983e1cd9a8aa2d31f2cb0e2787b
alegil0206 <a.gilardi7@campus.unimib.it>
2023-02-07 20:40:26 +0100

    Update ExecuteRecipeController.java

--- backend/brew-day-backend/src/main/java/unimib/ingsof/logic/ExecuteRecipeController.java
+++ backend/brew-day-backend/src/main/java/unimib/ingsof/logic/ExecuteRecipeController.java
@@ -6,10 +6,7 @@ import java.util.TreeMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import unimib.ingsof.exceptions.DoesntExistsException;
 import unimib.ingsof.exceptions.NotEnoughIngredientsException;
-import unimib.ingsof.exceptions.ValidationException;
-import unimib.ingsof.exceptions.WrongIDGenerationInitialization;
 import unimib.ingsof.persistence.service.Protocol;
 import unimib.ingsof.persistence.view.RecipeIngredientView;
 import unimib.ingsof.persistence.view.RecipeView;
@@ -23,7 +20,7 @@ public class ExecuteRecipeController {
 	@Autowired
 	ShoppingController shoppingController;
 
-	public void execute(String recipeID, float multiplier) throws NotEnoughIngredientsException, DoesntExistsException, ValidationException, WrongIDGenerationInitialization {
+	public void execute(String recipeID, float multiplier) throws Exception {
 		if (!shoppingController.getShoppingList(recipeID, multiplier).isEmpty()) 
 			throw new NotEnoughIngredientsException();
 		RecipeView recipe = recipeController.getRecipeByID(recipeID);
