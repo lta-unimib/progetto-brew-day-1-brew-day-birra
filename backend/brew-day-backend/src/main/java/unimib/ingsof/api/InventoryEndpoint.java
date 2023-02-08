7da411eab85a557a1bec5706c9bbba872ceb34d9
alegil0206 <a.gilardi7@campus.unimib.it>
2023-02-08 11:41:46 +0100

    uso di Protocol in InventoryEndpoint

--- backend/brew-day-backend/src/main/java/unimib/ingsof/api/InventoryEndpoint.java
+++ backend/brew-day-backend/src/main/java/unimib/ingsof/api/InventoryEndpoint.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import unimib.ingsof.logic.InventoryController;
+import unimib.ingsof.persistence.service.Protocol;
 import unimib.ingsof.persistence.view.IngredientView;
 
 @RestController
@@ -38,7 +39,7 @@ public class InventoryEndpoint {
 		try {
 			String ingredientID = inventoryController.addIngredient(ingredientObject);
 			HttpHeaders headers = new HttpHeaders();
-			headers.add("ingredientID", ingredientID);
+			headers.add(Protocol.INGREDIENT_ID_HEADER_KEY, ingredientID);
 			return new ResponseEntity<>(headers, HttpStatus.CREATED);
 		} catch(Exception exception) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
