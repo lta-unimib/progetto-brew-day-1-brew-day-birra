87f441f0bbbbdc8e53a0162b7890a0543768d8cc
alegil0206 <a.gilardi7@campus.unimib.it>
2023-02-08 11:32:57 +0100

    Update Protocol.java

--- backend/brew-day-backend/src/main/java/unimib/ingsof/persistence/service/Protocol.java
+++ backend/brew-day-backend/src/main/java/unimib/ingsof/persistence/service/Protocol.java
@@ -1,13 +1,20 @@
 package unimib.ingsof.persistence.service;
 
 public class Protocol {
-	public static final String NAME_KEY = "name";
-	public static final String QUANTITY_KEY = "quantity";
-	public static final String RECIPE_ID_KEY = "recipeID";
-	public static final String SETTING_ID_KEY = "settingID";
-	public static final String VALUE_KEY = "value";
-	public static final String DESCRIPTION_KEY = "description";
-	public static final String NOTETYPE_KEY = "noteType";
+	public static final String NAME_BODY_KEY = "name";
+	public static final String QUANTITY_BODY_KEY = "quantity";
+	public static final String RECIPE_ID_BODY_KEY = "recipeID";
+	public static final String DESCRIPTION_BODY_KEY = "description";
+	public static final String NOTETYPE_BODY_KEY = "noteType";	
+	
+	public static final String RECIPE_ID_HEADER_KEY = "recipeID";
+	public static final String BEER_ID_HEADER_KEY = "beerID";
+	public static final String NOTE_ID_HEADER_KEY = "noteID";
+	public static final String INGREDIENT_ID_HEADER_KEY = "ingredientID";
+	public static final String SETTING_ID_HEADER_KEY = "settingID";
+
+	public static final String SETTING_ID_BODY_KEY = "settingID";
+	public static final String VALUE_BODY_KEY = "value";
 	public static final String DEFAULT_EQUIPMENT = "30";
 	public static final String EQUIPMENT_SETTING_ID = "equipment";
 	
