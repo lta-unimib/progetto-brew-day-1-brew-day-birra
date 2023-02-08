8e39e7af5ca930b0c60e5ce3199ae14d2912b9db
qazimtoska <q.toska@campus.unimib.it>
2023-02-08 19:35:02 +0100

    Home now shows what beer you should execute (advice)

--- frontend/brew-day/src/pages/Home.jsx
+++ frontend/brew-day/src/pages/Home.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
+import RecipeView from "../components/RecipeView";
 
-const Home = () => {
-  return (
-    <div>
-    </div>
-  );
-};
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      advice: {},
+    };
+  }
+  componentDidMount() {
+    console.log('componentDidMount')
+    fetch("/api/advice")
+      .then((res) => res.json())
+      .then((data) => this.setState({ advice: data }));
+  }
+  render() {
+    console.log('render')
+    return (
+      <div>
+        <center>
+          <h1 id="advice-h1">Vuoi un consiglio? Ecco quale birra dovresti preparare</h1>
+          {Object.keys(this.state.advice).length === 0 ? null : (
+            <RecipeView recipeID={this.state.advice.recipeID} />
+          )}
+        </center>
+      </div>
+    );
+  }
+}
 
 export default Home;
