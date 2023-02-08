6bf41e13615fb1392293e768f1a59b35d424f209
alegil0206 <a.gilardi7@campus.unimib.it>
2023-02-08 11:38:00 +0100

    uso di Protocol in DescriptionDefaultingValidationHandler

--- backend/brew-day-backend/src/main/java/unimib/ingsof/validation/handlers/DescriptionDefaultingValidationHandler.java
+++ backend/brew-day-backend/src/main/java/unimib/ingsof/validation/handlers/DescriptionDefaultingValidationHandler.java
@@ -8,7 +8,7 @@ import unimib.ingsof.persistence.service.Protocol;
 public class DescriptionDefaultingValidationHandler extends BaseValidationHandler {
 	@Override
 	public Map<String, String> handle(Map<String, String> object) throws ValidationException {
-		object.computeIfAbsent(Protocol.DESCRIPTION_KEY, t -> "");
+		object.computeIfAbsent(Protocol.DESCRIPTION_BODY_KEY, t -> "");
 		return super.handle(object);
 	}
 }
