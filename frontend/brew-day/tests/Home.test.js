a7faa35cfd8f603086502b0e4b70eb35d7ed4924
qazimtoska <q.toska@campus.unimib.it>
2023-02-10 15:38:48 +0100

    Next recipe is implemented in Home / tests ok

--- frontend/brew-day/tests/Home.test.js
+++ frontend/brew-day/tests/Home.test.js
@@ -6,33 +6,37 @@ import Home from "../src/pages/Home";
 global.fetch = jest.fn().mockImplementation((url) =>
   Promise.resolve({
     json: () => {
-        if (url == "/api/advice")
-          return Promise.resolve({
-            recipeID: "recipe1",
-            quantity: 30.0
+      if (url == "/api/advice")
+        return Promise.resolve({
+          recipeID: "recipe1",
+          quantity: 30.0,
         });
-        else if (url == "/api/recipes/recipe1") {
-            return Promise.resolve({
+      else if (url == "/api/recipes/recipe1") {
+        return Promise.resolve({
+          recipeID: "recipe1",
+          name: "ricetta luppoli",
+          description: "test",
+          ingredients: [
+            {
+              ingredientID: "ingredient1",
+              name: "luppoli",
+              quantity: 3.0,
               recipeID: "recipe1",
-              name: "ricetta luppoli",
-              description: "test",
-              ingredients: [{
-                  ingredientID: "ingredient1",
-                  name: "luppoli",
-                  quantity: 3.0,
-                  recipeID: "recipe1"
-              }]
-          });
-        } else {
-          return Promise.resolve([]);
-        }
+            },
+          ],
+        });
+      } else if (url == "/api/shopping/recipe1") {
+        return Promise.resolve([]);
+      } else if (url == "/api/settings/nextRecipeID") {
+        return Promise.resolve({ value: "recipe1" });
+      }
     },
   })
-)
+);
 
 describe("Home component", () => {
   test("If response is not null, show recipe to execute", async () => {
     await act(() => render(<Home />));
-    expect(screen.getByText("Crea")).toBeInTheDocument();
+    expect(screen.getAllByText("Crea")[0]).toBeInTheDocument();
   });
 });
