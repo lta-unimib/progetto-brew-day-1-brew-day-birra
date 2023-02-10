bb68e95ab2548049276a3c03499b7a36748d8d03
frefolli <f.refolli@campus.unimib.it>
2023-02-10 22:02:13 +0100

    wrong test data

--- frontend/brew-day/tests/Impostazioni.resetAllSettings.test.js
+++ frontend/brew-day/tests/Impostazioni.resetAllSettings.test.js
@@ -4,18 +4,23 @@ import { render, screen, fireEvent, waitFor, getByRole } from "@testing-library/
 import Impostazioni from "../src/pages/Impostazioni";
 import { act } from "react-test-renderer";
 
+global.fetch = jest.fn().mockImplementation((url) =>
+  Promise.resolve({
+    json: () => [],
+  })
+)
 
 describe('Impostazioni.jsx can reset Settings', () => {
-    test('open resetNextRecipeID but dont reset', async () => {
+    test('open but dont reset', async () => {
         await act(() => {render(<Impostazioni/>);});
-        await act(() => {fireEvent.click(screen.getAllByText("Resetta la prossima ricetta da eseguire")[0])});
+        await act(() => {fireEvent.click(screen.getAllByText("Elimina tutti i dati")[0])});
         expect(screen.getByText("Sei sicuro di voler rimuovere tutti i tuoi dati?", {exact: false})).toBeInTheDocument();
         await act(() => {fireEvent.click(screen.getAllByText("Cancel")[0])});
     })
     
-    test('open resetNextRecipeID and reset', async () => {
+    test('open and reset', async () => {
         await act(() => {render(<Impostazioni/>);});
-        await act(() => {fireEvent.click(screen.getAllByText("Sei sicuro di voler rimuovere tutti i tuoi dati?")[0])});
+        await act(() => {fireEvent.click(screen.getAllByText("Elimina tutti i dati")[0])});
         //delete recipes.recipeID;
         await act(() => {fireEvent.click(screen.getByText("Conferma"))});
         expect(screen.queryByText("70")).toBeNull();
--- frontend/brew-day/tests/Impostazioni.resetNextRecipeID.test.js
+++ frontend/brew-day/tests/Impostazioni.resetNextRecipeID.test.js
@@ -1,26 +1,14 @@
 import "@testing-library/jest-dom/extend-expect";
 import React from "react";
-import { render, screen, fireEvent, waitFor, getByRole } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Impostazioni from "../src/pages/Impostazioni";
 import { act } from "react-test-renderer";
 
-/*
 global.fetch = jest.fn().mockImplementation((url) =>
   Promise.resolve({
-    json: () => {
-        if (url == "/api/recipes")
-          return Promise.resolve(Object.keys(recipes));
-        else {
-            if (url.startsWith("/api/recipes/")) {
-                let recipeID = url.replace("/api/recipes/", "");
-                return Promise.resolve(recipes[recipeID]);
-            } else {
-                return Promise.resolve(null);
-            }
-        }
-    },
+    json: () => [],
   })
-)*/
+)
 
 describe('Impostazioni.jsx can reset NextRecipeID', () => {
     test('open resetNextRecipeID but dont reset', async () => {
@@ -33,7 +21,6 @@ describe('Impostazioni.jsx can reset NextRecipeID', () => {
     test('open resetNextRecipeID and reset', async () => {
         await act(() => {render(<Impostazioni/>);});
         await act(() => {fireEvent.click(screen.getAllByText("Resetta la prossima ricetta da eseguire")[0])});
-        //delete recipes.recipeID;
         await act(() => {fireEvent.click(screen.getByText("Conferma"))});
     })
 })
