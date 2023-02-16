f219cc6630dee631c5cf9520a36aad0eef486718
qazimtoska <q.toska@campus.unimib.it>
2023-02-16 19:41:36 +0100

    Plus icon in Ricette

--- frontend/src/components/RecipeTable.jsx
+++ frontend/src/components/RecipeTable.jsx
@@ -2,6 +2,8 @@ import { TextField } from "@mui/material";
 import React from "react";
 import DedicatedTable from "./DedicatedTable";
 import MButton from "./MButton";
+import IconButton from '@mui/material/IconButton';
+import AddIcon from '@mui/icons-material/Add';
 
 const COLUMNS = [
     {title: "Nome", key: "name"},
@@ -26,7 +28,7 @@ export default class RecipeTable extends React.Component {
         const pivotRow = {
             name: (<TextField label="Recipe Name" value={this.props.newRecipeName} style={{width: "90%", textAlign:"center"}} onChange={ (event) => this.props.setNewRecipeName(event)}/>),
             description: (<TextField multiline label="Recipe Description" value={this.props.newRecipeDescription} style={{width: "90%", textAlign:"center"}} onChange={ (event) => this.props.setNewRecipeDescription(event)}/>),
-            toolbox: (<MButton text="Aggiungi" onClick={() => this.props.addRecipe()} />)
+            toolbox: (<IconButton aria-label="Aggiungi" onClick={() => this.props.addRecipe()}><AddIcon /></IconButton>)
         };
 
         return <DedicatedTable rows={rows} columns={COLUMNS} pivotRow={pivotRow}/>
