82f1f88b44e2f69aa41bdcb6c1f630fe7b29f964
Francesco Refolli <102532146+frefolli@users.noreply.github.com>
2023-02-18 19:34:55 +0100

    Update ShoppingEndpoint.java

--- backend/src/main/java/unimib/ingsof/api/ShoppingEndpoint.java
+++ backend/src/main/java/unimib/ingsof/api/ShoppingEndpoint.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import unimib.ingsof.exceptions.DoesntExistsException;
-import unimib.ingsof.exceptions.InternalServerException;
 import unimib.ingsof.exceptions.ValidationException;
 import unimib.ingsof.exceptions.WrongIDGenerationInitialization;
 import unimib.ingsof.logic.ShoppingController;
